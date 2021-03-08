00b3d24dfc70bc4635879a70d29261d03ccdca41
yl4869 <72660610+yl4869@users.noreply.github.com>
2021-03-08 15:40:57 +0800

    这是第一次试验提交
    耶

--- main.c
+++ main.c
@@ -1,27 +1,56 @@
 /*定义了列的大小为120，所以定义标准输出格式框架为80个‘*’号*/
-#define FRAME_ "\t\t\t\t*********************************************************\n"
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
-void print_user();
+#include "newhead.h"
+int main()
+{
+	system("mode con cols=120 lines=35"); // 定义好行和列的大小
+	HideCursor();
+	user_login();
+}
+//一个获取登陆命令，返回值为一个命令
+int get_login_cmd ()
+{
+    int i;
+    char ch;
+    while(scanf("%d",&i) != 1 || (i<0 || i>4)) {
+            while((ch = getchar()) != '\n') putchar(ch);
+            if(i<0 || i>4) printf("%d",i);
+            printf("不是一个有效的命令，请重新输入\n");
+            printf("请输入命令，按回车结束\n");
+    }
+    return i;
+}
+//退出函数
+void user_exit() {
+    printf("感谢您的使用，再见\n");
+}
+//用户登陆函数
 void user_login()
 {
-	int i;
+    int i;
 	print_user();
-	printf("请输入命令：\n");
-	scanf("%d", &i);
+	printf("请输入命令,按回车结束：\n");
+	switch(get_login_cmd()) {
+        case 0: user_exit();  break;//exit(); break;
+        case 1: break; //调用登陆函数
+        case 2: break; //调用注册新账号函数
+        case 3: break; //调用修改密码函数
+        case 4: break ; //
+    }
 }
+//打印登陆框架
 void print_user()
 {
 	printf("\n\n\n\n\n\n");
 	printf("\t\t\t\t\t\t欢迎进入学生成绩管理系统\t\t\n");
 	printf(FRAME_);
-	//printf("\t\t\t\t*\t\t欢迎进入学生成绩管理系统\t\t*\n");
-	printf("\t\t\t\t*\t\t    登录 请按 1\t\t\t\t*\n");
-	printf("\t\t\t\t*\t\t    注册新账号 请按 2\t\t\t*\n");
-	printf("\t\t\t\t*\t\t    找回密码 请按 3\t\t\t*\n");
-	printf("\t\t\t\t*\t\t    修改密码 请按 4\t\t\t*\n");
-	printf("\t\t\t\t*\t\t    退出程序 请按 5\t\t\t*\n");
+	printf("\t\t\t\t*\t\t    登录 \t请按 1\t\t\t*\n");
+	printf("\t\t\t\t*\t\t    注册新账号 \t请按 2\t\t\t*\n");
+	printf("\t\t\t\t*\t\t    找回密码 \t请按 3\t\t\t*\n");
+	printf("\t\t\t\t*\t\t    修改密码 \t请按 4\t\t\t*\n");
+	printf("\t\t\t\t*\t\t    退出程序 \t请按 0\t\t\t*\n");
 	printf(FRAME_);
 }
 //隐藏光标
@@ -32,10 +61,4 @@ void HideCursor()
 	GetConsoleCursorInfo(hout, &cci);
 	cci.bVisible = FALSE;
 	SetConsoleCursorInfo(hout, &cci);
-}
-int main()
-{
-	system("mode con cols=120 lines=35"); // 定义好行和列的大小
-	HideCursor();
-	user_login();
 }
---
+++ newhead.h
@@ -0,0 +1,10 @@
+#ifndef NEWHEAD_H_INCLUDED
+#define NEWHEAD_H_INCLUDED
+#define FRAME_ "\t\t\t\t*********************************************************\n"
+void print_user();
+int get_login_cmd ();
+void exit();
+void user_login();
+void print_user();
+void HideCursor();
+#endif // NEWHEAD_H_INCLUDED
